eab1c0d4df8e05426c14319c0c3c69639c8d0de4
szymon <szymon.kapala@swmansion.com>
2020-07-20 09:03:13 +0200

    fresh 63.1

--- android/app/src/main/java/com/reanimated2playground/MainApplication.java
+++ android/app/src/main/java/com/reanimated2playground/MainApplication.java
@@ -10,15 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.facebook.react.config.ReactFeatureFlags;
-import com.facebook.react.bridge.JSIModulePackage;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
-
-  static {
-    ReactFeatureFlags.useTurboModules = true;
-  }
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
@@ -39,11 +32,6 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
-        }
-
-        @Override
-        protected JSIModulePackage getJSIModulePackage() {
-          return new ReanimatedJSIModulePackage();
         }
       };
 
