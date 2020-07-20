71642dbe7bd96eb41df5b9f59d661ab15f6fc3f8
szymon <szymon.kapala@swmansion.com>
2020-07-20 09:29:32 +0200

    Android

--- android/app/src/main/java/com/reanimated2playground/MainApplication.java
+++ android/app/src/main/java/com/reanimated2playground/MainApplication.java
@@ -7,7 +7,10 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JSIModulePackage;
 import com.facebook.soloader.SoLoader;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -32,6 +35,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage(); // <- add
         }
       };
 
