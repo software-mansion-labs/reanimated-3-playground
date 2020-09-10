cc31fbfc24b8b4ae2b748fb79bf173aeededab29
karol-bisztyga <karol.bisztyga@swmansion.com>
2020-09-10 15:10:58 +0200

    init

--- App.js
+++ App.js
@@ -1,44 +1,12 @@
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from 'react-native-reanimated';
-import {View, Button} from 'react-native';
-import React from 'react';
+import {AppRegistry, Platform} from 'react-native';
+import {name as appName} from './app.json';
+import App from './Screen';
 
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
+AppRegistry.registerComponent(appName, () => App);
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
-
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: 'column',
-      }}>
-      <Animated.View
-        style={[
-          {width: 100, height: 80, backgroundColor: 'black', margin: 30},
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
-    </View>
-  );
+if (Platform.OS === 'web') {
+  const rootTag = document.getElementById('root');
+  AppRegistry.runApplication(appName, {rootTag});
 }
+
+export default App;
