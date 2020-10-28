379752cc9374622fd96e7cf2d337b079bc73bdcf
karol-bisztyga <karol.bisztyga@swmansion.com>
2020-10-28 10:15:12 +0100

    update app delegate

--- ios/Reanimated2Playground/AppDelegate.mm
+++ ios/Reanimated2Playground/AppDelegate.mm
@@ -4,22 +4,6 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
-#import <React/RCTCxxBridgeDelegate.h>
-#import <ReactCommon/RCTTurboModuleManager.h>
-
-// add headers (start)
-#import <React/RCTDataRequestHandler.h>
-#import <React/RCTFileRequestHandler.h>
-#import <React/RCTHTTPRequestHandler.h>
-#import <React/RCTNetworking.h>
-#import <React/RCTLocalAssetImageLoader.h>
-#import <React/RCTGIFImageDecoder.h>
-#import <React/RCTImageLoader.h>
-#import <React/JSCExecutorFactory.h>
-#import <RNReanimated/REATurboModuleProvider.h>
-#import <RNReanimated/REAModule.h>
-// add headers (end)
-
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -39,8 +23,7 @@ static void InitializeFlipper(UIApplication *application) {
 }
 #endif
 
-@interface AppDelegate() <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
-    RCTTurboModuleManager *_turboModuleManager;
+@interface AppDelegate() {
 }
 @end
 
@@ -48,7 +31,6 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  RCTEnableTurboModule(YES); // <- add
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
@@ -75,65 +57,6 @@ static void InitializeFlipper(UIApplication *application) {
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
-}
-
-- (std::unique_ptr<facebook::react::JSExecutorFactory>)jsExecutorFactoryForBridge:(RCTBridge *)bridge
-{
-  _bridge_reanimated = bridge;
-  _turboModuleManager = [[RCTTurboModuleManager alloc] initWithBridge:bridge
-                                                              delegate:self
-                                                             jsInvoker:bridge.jsCallInvoker];
- #if RCT_DEV
-  [_turboModuleManager moduleForName:"RCTDevMenu"]; // <- add
- #endif
- __weak __typeof(self) weakSelf = self;
- return std::make_unique<facebook::react::JSCExecutorFactory>([weakSelf, bridge](facebook::jsi::Runtime &runtime) {
-   if (!bridge) {
-     return;
-   }
-   __typeof(self) strongSelf = weakSelf;
-   if (strongSelf) {
-     [strongSelf->_turboModuleManager installJSBindingWithRuntime:&runtime];
-   }
- });
-}
-
-- (Class)getModuleClassFromName:(const char *)name
-{
- return facebook::react::REATurboModuleClassProvider(name);
-}
-
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
-                                                     jsInvoker:(std::shared_ptr<facebook::react::CallInvoker>)jsInvoker
-{
- return facebook::react::REATurboModuleProvider(name, jsInvoker);
-}
-
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
-                                                      instance:(id<RCTTurboModule>)instance
-                                                     jsInvoker:(std::shared_ptr<facebook::react::CallInvoker>)jsInvoker
-{
- return facebook::react::REATurboModuleProvider(name, instance, jsInvoker);
-}
-
-- (id<RCTTurboModule>)getModuleInstanceFromClass:(Class)moduleClass
-{
- if (moduleClass == RCTImageLoader.class) {
-   return [[moduleClass alloc] initWithRedirectDelegate:nil loadersProvider:^NSArray<id<RCTImageURLLoader>> *{
-     return @[[RCTLocalAssetImageLoader new]];
-   } decodersProvider:^NSArray<id<RCTImageDataDecoder>> *{
-     return @[[RCTGIFImageDecoder new]];
-   }];
- } else if (moduleClass == RCTNetworking.class) {
-   return [[moduleClass alloc] initWithHandlersProvider:^NSArray<id<RCTURLRequestHandler>> *{
-     return @[
-       [RCTHTTPRequestHandler new],
-       [RCTDataRequestHandler new],
-       [RCTFileRequestHandler new],
-     ];
-   }];
- }
- return [moduleClass new];
 }
 
 @end
